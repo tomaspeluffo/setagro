efdc1441956b5c3565da245b72503855c2363b80
tomaspe1 <tomaspeluffo@gmail.com>
2020-07-31 10:09:41 -0300

    arreglamos los links

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -5,7 +5,7 @@ const Social = () => {
         <div className="icons">
             <ul>
                <a href="https://www.facebook.com/watch/setideas/" target="_blank" rel="noopener noreferrer"><li className="facebook"><img src="../imagenes/logos/facebook2.svg" alt="logo facebook"/></li></a>
-               <a href="#!"><li className="instagram"><img src="../imagenes/logos/instagram.svg" alt="logo instagram"/></li></a>
+               <a href="https://www.instagram.com/set.ideas/" target="_blank" rel="noopener noreferrer"><li className="instagram"><img src="../imagenes/logos/instagram.svg" alt="logo instagram"/></li></a>
                <a href="#!"><li className="whatsapp"><img src="../imagenes/logos/whatsapp.svg" alt="logo whatsapp"/></li></a>
             </ul>
         </div>
